3e8b4a8d36f493040d016fbeeb237b874b05cfa6
r-kioko <kioko@usc.edu>
2024-04-02 14:45:40 -0400

    removing js for google map shelter list

--- main.js
+++ main.js
@@ -7,37 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// document.getElementById('zip-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     var zipCode = document.getElementById('zip').value;
-
-//     // Step 1: Use Nominatim API to get coordinates from zip code
-//     fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + zipCode)
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data.length > 0) {
-//                 var latitude = data[0].lat;
-//                 var longitude = data[0].lon;
-
-//                 // Step 2: Use Overpass API to find nearby shelters
-//                 fetch('https://overpass-api.de/api/interpreter?data=[out:json];node["amenity"="animal_shelter"](around:5000,' + latitude + ',' + longitude + ');out;')
-//                     .then(response => response.json())
-//                     .then(data => {
-//                         displayShelters(data.elements);
-//                     })
-//                     .catch(error => {
-//                         console.error('Error fetching nearby shelters:', error);
-//                     });
-//             } else {
-//                 console.error('No coordinates found for the zip code:', zipCode);
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Error fetching coordinates:', error);
-//         });
-// });
-
 function displayShelters(shelters) {
   var shelterList = document.getElementById('shelter-list');
   shelterList.innerHTML = ''; // Clear previous results
